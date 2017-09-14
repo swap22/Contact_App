2c9c19ff4efdb31e2a48795b2ab5fd5ec3a805b0
Swapnil Sharma <sharma.sswapnil@gmail.com>
2017-09-14 19:51:12 +0530

    Demo of $http service through json-server
    Here I have added code for $http service into contact controller. This is
    not ideal way to do it as we need service to get data form server and not
    the controller but this is just for demo and we can see how the
    functionality works with $http.

--- contact.controller.js
+++ contact.controller.js
@@ -2,10 +2,15 @@
     var app = angular.module("ContactApp");
     app.controller("ContactCtrl", Contact);
 
-    function Contact(ContactDataSvc) {
-        this.contact = ContactDataSvc.contacts;
-        this.selectedUser = this.contact[0];
-        this.selectUser = function (index) {
+    function Contact(ContactDataSvc, $http) {
+        var self = this;
+
+        $http.get('http://localhost:3000/contacts')
+        .then(function(response){
+            self.contact = response.data;
+        });
+
+         this.selectUser = function (index) {
             this.selectedUser = this.contact[index];
         }
     }
--- contactdata.service.js
+++ contactdata.service.js
@@ -1,33 +1,6 @@
 (function(){
     var app = angular.module("ContactApp");
     app.service("ContactDataSvc", function(){
-        this.contacts = [
-            {
-                "gender": "male",
-                "name": {
-                    "title": "Mr.",
-                    "first": "Romain",
-                    "last": "Hoogmoed"
-                },
-                "picture": {
-                    "large": "https://randomuser.me/api/portraits/men/83.jpg",
-                    "medium": "https://randomuser.me/api/portraits/med/men/83.jpg",
-                    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/83.jpg"
-                }
-            },
-            {
-                "gender": "male",
-                "name": {
-                    "title": "Mr.",
-                    "first": "John",
-                    "last": "Williams"
-                },
-                "picture": {
-                    "large": "https://randomuser.me/api/portraits/men/84.jpg",
-                    "medium": "https://randomuser.me/api/portraits/med/men/84.jpg",
-                    "thumbnail": "https://randomuser.me/api/portraits/thumb/men/84.jpg"
-                }
-            }
-        ];
+        this.contacts = [];
     });
 })();
